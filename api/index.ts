4f19bf4339f98b306db9fa0d77ce13528bab3ff7
aloky06 <aloky06@gmail.com>
2026-06-22 17:47:03 +0530

    Implement lazy loading Serverless pattern for Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,23 +1,30 @@
+// @ts-nocheck
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-
 import express from 'express';
 
-const expressApp = express();
+const server = express();
+
+let cachedServer: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
-  
-  app.setGlobalPrefix('api');
-  await app.init();
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    app.enableCors({
+      origin: true,
+      credentials: true,
+    });
+    
+    app.setGlobalPrefix('api');
+    await app.init();
+    cachedServer = server;
+  }
+  return cachedServer;
 }
 
-bootstrap();
-
-export default expressApp;
+export default async function handler(req: any, res: any) {
+  const app = await bootstrap();
+  return app(req, res);
+}
