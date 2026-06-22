3712813ce0be243578b16c87dbdc2ce21ea206ea
aloky06 <aloky06@gmail.com>
2026-06-22 16:09:06 +0530

    Restore api/index.ts for Vercel deployment

---
+++ api/index.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express = require('express');
+
+const expressApp = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+  
+  app.setGlobalPrefix('api');
+  await app.init();
+}
+
+bootstrap();
+
+export default expressApp;
